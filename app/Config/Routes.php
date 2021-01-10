f748ada9ba1c9d7f6e972928e7a11f41bc2f9977
yosi kats <yoskats620@gmail.com>
2021-01-10 17:17:53 +0200

    routes 6

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -17,7 +17,7 @@ if (file_exists(SYSTEMPATH . 'Config/Routes.php'))
  */
 $routes->setDefaultNamespace('App\Controllers');
 $routes->setDefaultController('Phonebook');
-$routes->setDefaultMethod('index');
+$routes->setDefaultMethod('home');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
 $routes->setAutoRoute(true);
