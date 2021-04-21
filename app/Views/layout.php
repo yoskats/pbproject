aff339fcc08f2621339c282b115f0af37fdc5414
yosi kats <yoskats620@gmail.com>
2021-04-21 14:09:24 +0300

    Revert "update"
    This reverts commit 4b2df53dacbe0cbb9f6ee777d232b59326054cc4.

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -12,7 +12,7 @@
 </head>
 
 <body class="bg-light">
-    <?php helper('auth'); ?>
+    <?php helper('auth');?>
     <?= view('Views\navbar') ?>
     <?= $this->renderSection('main') ?>
 
@@ -21,7 +21,7 @@
     <div class="footer col-12 col-md-8 offset-md-2 pt-5 bg-dark">
         <footer class="footer">
             <!-- Copyright -->
-            <div class="footer-copyright text-center py-3 text-white mb-5">© 2020 Copyright saved to : <a class="text-danger text-center" href="">yoskats620@gmail.com_get_active_object </a>
+            <div class="footer-copyright text-center py-3 text-white mb-5">© 2020 Copyright saved to : <a class="text-danger text-center" href="">yosi@123code.co </a>
                 <a class="text-white text-center" href="/home"> Yosi's Phonebook</a>
             </div>
             <!-- Copyright -->
--- system/Debug/Toolbar/Views/toolbar.tpl.php
+++ system/Debug/Toolbar/Views/toolbar.tpl.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @var \CodeIgniter\Debug\Toolbar $this
  * @var int                        $totalTime
@@ -27,7 +26,26 @@
 <script id="toolbar_js" type="text/javascript">
 	<?= file_get_contents(__DIR__ . '/toolbar.js') ?>
 </script>
-
+<div id="debug-icon" class="debug-bar-ndisplay">
+	<a id="debug-icon-link" href="javascript:void(0)">
+		<svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+			width="155.000000px" height="200.000000px" viewBox="0 0 155.000000 200.000000"
+			preserveAspectRatio="xMidYMid meet">
+			<g transform="translate(0.000000,200.000000) scale(0.100000,-0.100000)" fill="#dd4814" stroke="none">
+				<path d="M737 1963 c22 -79 -7 -185 -78 -290 -18 -26 -107 -122 -197 -213
+				-239 -240 -336 -371 -403 -544 -79 -206 -78 -408 5 -582 64 -134 212 -264 361
+				-314 l60 -20 -30 22 c-210 152 -229 387 -48 588 25 27 48 50 51 50 4 0 7 -27
+				7 -61 0 -57 2 -62 37 -95 30 -27 46 -34 78 -34 56 0 99 24 116 65 29 69 16
+				120 -50 205 -105 134 -117 233 -43 347 l31 48 7 -47 c13 -82 58 -129 250 -258
+				209 -141 306 -261 328 -405 11 -72 -1 -161 -31 -218 -27 -53 -112 -143 -165
+				-174 -24 -14 -43 -26 -43 -28 0 -2 24 4 53 14 241 83 427 271 482 486 19 76
+				19 202 -1 285 -35 152 -146 305 -299 412 l-70 49 -6 -33 c-8 -48 -26 -76 -59
+				-93 -45 -23 -103 -19 -138 10 -67 57 -78 146 -37 305 30 116 32 206 5 291 -27
+				89 -104 206 -162 247 -17 13 -18 12 -11 -15z"/>
+			</g>
+		</svg>
+	</a>
+</div>
 <div id="debug-bar">
 	<div class="toolbar">
 		<span id="toolbar-position"><a href="javascript: void(0)">&#8597;</a></span>
@@ -40,13 +58,13 @@
 		</span>
 
 		<?php foreach ($collectors as $c) : ?>
-			<?php if (!$c['isEmpty'] && ($c['hasTabContent'] || $c['hasLabel'])) : ?>
+			<?php if (! $c['isEmpty'] && ($c['hasTabContent'] || $c['hasLabel'])) : ?>
 				<span class="ci-label">
 					<a href="javascript: void(0)" data-tab="ci-<?= $c['titleSafe'] ?>">
 						<img src="<?= $c['icon'] ?>">
 						<span class="hide-sm">
 							<?= $c['title'] ?>
-							<?php if (!is_null($c['badgeValue'])) : ?>
+							<?php if (! is_null($c['badgeValue'])) : ?>
 								<span class="badge"><?= $c['badgeValue'] ?></span>
 							<?php endif ?>
 						</span>
@@ -65,7 +83,9 @@
 		<h1>
 			<span class="ci-label">
 				<a href="javascript: void(0)" data-tab="ci-config">
-					<svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="18.60px" height="24.0px" viewBox="0 0 18.60 28.000000" preserveAspectRatio="xMidYMid meet">
+					<svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+						width="18.60px" height="24.0px" viewBox="0 0 18.60 28.000000"
+						preserveAspectRatio="xMidYMid meet">
 						<g transform="translate(0.000000,28.000000) scale(0.010000,-0.010000)" fill="#dd4814" stroke="none">
 							<path d="M737 1963 c22 -79 -7 -185 -78 -290 -18 -26 -107 -122 -197 -213
 							-239 -240 -336 -371 -403 -544 -79 -206 -78 -408 5 -582 64 -134 212 -264 361
@@ -76,7 +96,7 @@
 							-174 -24 -14 -43 -26 -43 -28 0 -2 24 4 53 14 241 83 427 271 482 486 19 76
 							19 202 -1 285 -35 152 -146 305 -299 412 l-70 49 -6 -33 c-8 -48 -26 -76 -59
 							-93 -45 -23 -103 -19 -138 10 -67 57 -78 146 -37 305 30 116 32 206 5 291 -27
-							89 -104 206 -162 247 -17 13 -18 12 -11 -15z" />
+							89 -104 206 -162 247 -17 13 -18 12 -11 -15z"/>
 						</g>
 					</svg>
 					<?= $CI_VERSION ?>
@@ -94,30 +114,25 @@
 	<div id="ci-timeline" class="tab">
 		<table class="timeline">
 			<thead>
-				<tr>
-					<th class="debug-bar-width30">NAME</th>
-					<th class="debug-bar-width10">COMPONENT</th>
-					<th class="debug-bar-width10">DURATION</th>
-					<?php for ($i = 0; $i < $segmentCount; $i++) : ?>
-						<th><?= $i * $segmentDuration ?> ms</th>
-					<?php endfor ?>
-				</tr>
+			<tr>
+				<th class="debug-bar-width30">NAME</th>
+				<th class="debug-bar-width10">COMPONENT</th>
+				<th class="debug-bar-width10">DURATION</th>
+				<?php for ($i = 0; $i < $segmentCount; $i++) : ?>
+					<th><?= $i * $segmentDuration ?> ms</th>
+				<?php endfor ?>
+			</tr>
 			</thead>
 			<tbody>
-				<?= $this->renderTimeline(
-					$collectors,
-					$startTime,
-					$segmentCount,
-					$segmentDuration,
-					$styles
-				) ?>
+			<?= $this->renderTimeline($collectors, $startTime, $segmentCount, $segmentDuration,
+				$styles) ?>
 			</tbody>
 		</table>
 	</div>
 
 	<!-- Collector-provided Tabs -->
 	<?php foreach ($collectors as $c) : ?>
-		<?php if (!$c['isEmpty']) : ?>
+		<?php if (! $c['isEmpty']) : ?>
 			<?php if ($c['hasTabContent']) : ?>
 				<div id="ci-<?= $c['titleSafe'] ?>" class="tab">
 					<h2><?= $c['title'] ?> <span><?= $c['titleDetails'] ?></span></h2>
@@ -135,11 +150,8 @@
 		<?php if (isset($vars['varData'])) : ?>
 			<?php foreach ($vars['varData'] as $heading => $items) : ?>
 
-				<a href="javascript:void(0)" onclick="ciDebugBar.toggleDataTable('<?= strtolower(str_replace(
-																						' ',
-																						'-',
-																						$heading
-																					)) ?>'); return false;">
+				<a href="javascript:void(0)" onclick="ciDebugBar.toggleDataTable('<?= strtolower(str_replace(' ',
+					'-', $heading)) ?>'); return false;">
 					<h2><?= $heading ?></h2>
 				</a>
 
@@ -147,16 +159,16 @@
 
 					<table id="<?= strtolower(str_replace(' ', '-', $heading . '_table')) ?>">
 						<tbody>
-							<?php foreach ($items as $key => $value) : ?>
-								<tr>
-									<td><?= $key ?></td>
-									<td><?= $value ?></td>
-								</tr>
-							<?php endforeach ?>
+						<?php foreach ($items as $key => $value) : ?>
+							<tr>
+								<td><?= $key ?></td>
+								<td><?= $value ?></td>
+							</tr>
+						<?php endforeach ?>
 						</tbody>
 					</table>
 
-				<?php else : ?>
+				<?php else: ?>
 					<p class="muted">No data to display.</p>
 				<?php endif ?>
 			<?php endforeach ?>
@@ -168,15 +180,15 @@
 		</a>
 
 		<?php if (isset($vars['session'])) : ?>
-			<?php if (!empty($vars['session'])) : ?>
+			<?php if (! empty($vars['session'])) : ?>
 				<table id="session_table">
 					<tbody>
-						<?php foreach ($vars['session'] as $key => $value) : ?>
-							<tr>
-								<td><?= $key ?></td>
-								<td><?= $value ?></td>
-							</tr>
-						<?php endforeach ?>
+					<?php foreach ($vars['session'] as $key => $value) : ?>
+						<tr>
+							<td><?= $key ?></td>
+							<td><?= $value ?></td>
+						</tr>
+					<?php endforeach ?>
 					</tbody>
 				</table>
 			<?php else : ?>
@@ -195,12 +207,12 @@
 
 			<table id="get_table">
 				<tbody>
-					<?php foreach ($get as $name => $value) : ?>
-						<tr>
-							<td><?= $name ?></td>
-							<td><?= $value ?></td>
-						</tr>
-					<?php endforeach ?>
+				<?php foreach ($get as $name => $value) : ?>
+					<tr>
+						<td><?= $name ?></td>
+						<td><?= $value ?></td>
+					</tr>
+				<?php endforeach ?>
 				</tbody>
 			</table>
 		<?php endif ?>
@@ -212,12 +224,12 @@
 
 			<table id="post_table">
 				<tbody>
-					<?php foreach ($post as $name => $value) : ?>
-						<tr>
-							<td><?= $name ?></td>
-							<td><?= $value ?></td>
-						</tr>
-					<?php endforeach ?>
+				<?php foreach ($post as $name => $value) : ?>
+					<tr>
+						<td><?= $name ?></td>
+						<td><?= $value ?></td>
+					</tr>
+				<?php endforeach ?>
 				</tbody>
 			</table>
 		<?php endif ?>
@@ -229,12 +241,12 @@
 
 			<table id="request_headers_table">
 				<tbody>
-					<?php foreach ($headers as $header => $value) : ?>
-						<tr>
-							<td><?= $header ?></td>
-							<td><?= $value ?></td>
-						</tr>
-					<?php endforeach ?>
+				<?php foreach ($headers as $header => $value) : ?>
+					<tr>
+						<td><?= $header ?></td>
+						<td><?= $value ?></td>
+					</tr>
+				<?php endforeach ?>
 				</tbody>
 			</table>
 		<?php endif ?>
@@ -246,12 +258,12 @@
 
 			<table id="cookie_table">
 				<tbody>
-					<?php foreach ($cookies as $name => $value) : ?>
-						<tr>
-							<td><?= $name ?></td>
-							<td><?= is_array($value) ? print_r($value, true) : $value ?></td>
-						</tr>
-					<?php endforeach ?>
+				<?php foreach ($cookies as $name => $value) : ?>
+					<tr>
+						<td><?= $name ?></td>
+						<td><?= is_array($value) ? print_r($value, true) : $value ?></td>
+					</tr>
+				<?php endforeach ?>
 				</tbody>
 			</table>
 		<?php endif ?>
@@ -267,12 +279,12 @@
 
 			<table id="response_headers_table">
 				<tbody>
-					<?php foreach ($headers as $header => $value) : ?>
-						<tr>
-							<td><?= $header ?></td>
-							<td><?= $value ?></td>
-						</tr>
-					<?php endforeach ?>
+				<?php foreach ($headers as $header => $value) : ?>
+					<tr>
+						<td><?= $header ?></td>
+						<td><?= $value ?></td>
+					</tr>
+				<?php endforeach ?>
 				</tbody>
 			</table>
 		<?php endif ?>
@@ -286,7 +298,8 @@
 	</div>
 </div>
 <style type="text/css">
-	<?php foreach ($styles as $name => $style) : ?>.<?= $name ?> {
+	<?php foreach($styles as $name => $style) : ?>
+	.<?= $name ?> {
 		<?= $style ?>
 	}
 
