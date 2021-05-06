bd9840c9bcab12a043c3ae409a29ac04ecf9abbd
yosi kats <yoskats620@gmail.com>
2021-05-06 18:53:45 +0300

    update

--- app/Views/new_page.php
+++ app/Views/new_page.php
@@ -1,7 +1,7 @@
 <?= $this->extend('layout') ?>
 <?= $this->section('main') ?>
-
-    <div class="col-12 col-md-8 offset-md-2 mr-4 pt-5 pb-5">
+<div class="container new-section d-flex flex-column justify-content-center">
+    <div class="d-flex flex-column mx-auto col-sm-12 col-md-12 col-lg-10 col-xl-10 col-12 py-5">
         <div class="bg-secondary">
             <h1 class="text-light pt-5 pb-5 text-center">Add New Contact</h1>
             <h2 class="text-light pt-5 pb-5 text-center">add new contact to you'r Phonebook</h2>
@@ -9,7 +9,7 @@
     </div>
 
 
-    <div class="col-12 col-md-8 offset-md-2 pt-5 pb-5">
+    <div class="d-flex flex-column mx-auto col-sm-12 col-md-12 col-lg-10 col-xl-10 col-12 py-5">
         <?php if (!empty($errors)) : ?>
             <div class="alert alert-danger">
                 <?php foreach ($errors as $field => $error) : ?>
@@ -17,48 +17,48 @@
                 <?php endforeach ?>
             </div>
         <?php endif ?>
-        <form class="users_num" action="/new" method="post" style="padding-left: 200px; padding-right: 200px;">
-            <div class="row pt-5">
-                <div class="col">
+        <form class="users_num d-flex flex-column justify-content-center" action="/new" method="post">
+            <div class="row">
+                <div class="col-12">
 
                     <div class="row">
                         <div class="col" style="padding-bottom: 30px;">
-                            <input type="hidden" class="form-control h3" name="user_id" id="user_id"  value="<?php echo user_id(); ?>">
+                            <input type="hidden" class="form-control h3" name="user_id" id="user_id" value="<?php echo user_id(); ?>">
                         </div>
                     </div>
 
                     <div class="row">
-                        <div class="col" style="padding-bottom: 30px; padding-top: 8px;">
+                        <div class="col-4" style="padding-bottom: 30px; padding-top: 8px;">
                             <label class="h3">Full name</label>
                         </div>
-                        <div class="col" style="padding-bottom: 30px;">
+                        <div class="col-4" style="padding-bottom: 30px;">
                             <input type="text" class="form-control h3" name="name" id="name" placeholder="Full name" value="<?= set_value('name') ?>">
                         </div>
                     </div>
 
                     <div class="row">
-                        <div class="col" style="padding-bottom: 30px; padding-top: 8px;">
+                        <div class="col-4" style="padding-bottom: 30px; padding-top: 8px;">
                             <label class="h3">Phone number</label>
                         </div>
-                        <div class="col" style="padding-bottom: 30px;">
+                        <div class="col-4" style="padding-bottom: 30px;">
                             <input type="tel" class="form-control h3" name="phone" id="phone_num" placeholder="Mobile phone number" value="<?= set_value('phone_num') ?>">
                         </div>
                     </div>
 
                     <div class="row">
-                        <div class="col" style="padding-bottom: 30px; padding-top: 8px;">
+                        <div class="col-4" style="padding-bottom: 30px; padding-top: 8px;">
                             <label class="h3">Email</label>
                         </div>
-                        <div class="col" style="padding-bottom: 30px;">
+                        <div class="col-4" style="padding-bottom: 30px;">
                             <input type="text" class="form-control h3" name="email" id="email" placeholder="Email" value="<?= set_value('email') ?>">
                         </div>
                     </div>
 
                     <div class="row">
-                        <div class="col" style="padding-bottom: 30px; padding-top: 8px;">
+                        <div class="col-4" style="padding-bottom: 30px; padding-top: 8px;">
                             <label class="h3">Birthday Date</label>
                         </div>
-                        <div class="col" style="padding-bottom: 30px;">
+                        <div class="col-4" style="padding-bottom: 30px;">
                             <input type="date" class="form-control h3" name="bday" id="bday" placeholder="01.01.2020" value="<?= set_value('bday') ?>">
                         </div>
                     </div>
@@ -75,4 +75,5 @@
 
         </form>
     </div>
+</div>
 <?= $this->endSection() ?>
